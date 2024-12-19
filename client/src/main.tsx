655b23873fa9d85d28d14dc6429e27cb148c105c
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-12-19 18:54:33 +0100

    Ajouter la configuration de base pour React Router et Redux avec des commentaires pour l'intégration future

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux'
+import { store, persistor } from "./stores/store.ts";
+import { PersistGate } from 'redux-persist/integration/react'
 import App from './App.tsx'
+import "./scss/index.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      {/* <Provider store={store}> */}
+        {/* <PersistGate loading={null} persistor={persistor}> */}
+          <App />
+        {/* </PersistGate> */}
+      {/* </Provider> */}
+    </BrowserRouter>
   </StrictMode>,
 )
