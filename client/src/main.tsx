4e91d2b715a03be0574ca9170193e8bf025a5af4
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-12-21 08:41:36 +0100

    Corriger la syntaxe des importations dans le fichier main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux'
 import { store, persistor } from "./stores/store.ts";
-import { PersistGate } from 'redux-persist/integration/react'
-import App from './App.tsx'
+import { PersistGate } from 'redux-persist/integration/react';
+import App from './App.tsx';
 import "./scss/index.css";
 
 createRoot(document.getElementById('root')!).render(
