c47c26fdfb89b4f1fff56eca91dcaa39ca1be046
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-12-21 08:40:12 +0100

    Ajouter le composant Footer avec le texte de copyright

---
+++ client/src/pages/home/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import '../styles/Footer.scss';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 RéservApp. Tous droits réservés.</p>
+    </footer>
+  );
+};
+
+export default Footer;
