03d22874da7c24ab3315c3365161f25e6164a486
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-01-06 18:25:10 +0100

    Ajouter des slices pour les paramètres avancés, le menu, les notifications, le marketing, le paiement et l'accessibilité dans le store

--- client/src/stores/store.ts
+++ client/src/stores/store.ts
@@ -5,6 +5,13 @@ import localStorage from "redux-persist/lib/storage";
 import profileSlice from "../pages/profil/profileSlice";
 import horaires from "../pages/setting/horaires_&_policies/horaires/horairesSlices";
 import policies from "../pages/setting/horaires_&_policies/policies/policiesSlice";
+import advancedSettings from "../pages/setting/horaires_&_policies/advancedSettings/advancedSettingsSlice";
+import menuSettings from "../pages/setting/menu_&_carte/menuSettingsSlice";
+import notificationSetting from "../pages/setting/notification_&_marketing/notificationSettingSlice";
+import marketingSetting from "../pages/setting/notification_&_marketing/marketingSettingSlice";
+import paymentSettings from "../pages/setting/paiement_&_plan_de_salle/paiementSlice";
+import accessibilitySetting from "../pages/setting/accessibilité_&_autres/accessibilitySlice";
+
 
 const persistConfig = {
     key: "root",
@@ -17,6 +24,12 @@ const store = configureStore({
         profile: profileSlice,
         horaires: horaires,
         policies: policies,
+        advancedSettings: advancedSettings,
+        menuSettings: menuSettings,
+        notificationSetting: notificationSetting,
+        marketingSetting: marketingSetting,
+        paymentSettings: paymentSettings,
+        accessibilitySetting: accessibilitySetting,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
